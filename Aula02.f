8386c2964cfac5891d28f6c1953f823e1b616ae3
poca pika <126169808+rOBOCOP-GAy@users.noreply.github.com>
2023-02-28 11:56:54 -0300

    Create Aula02.f

---
+++ Aula02.f
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main(){
+
+runApp( MaterialApp(
+//debugShowCheckModeBanner: false;
+title: 'Aula 02 Flutter 2DS', 
+
+home: Container(
+color: Colors.white,
+//padding: EdgeInsets.fromLTRB( 0, 0, 0, 0)
+//margin: EdgeInsets.all (100)
+child: Column( children: <Widget> [
+
+        Padding( 
+        padding: EdgeInsets.fromLTRB( 0, 0, 0, 0),
+        child: Text('Sexodelicinha'),),
+        
+        Padding( 
+        padding: EdgeInsets.fromLTRB( 10, 10, 10, 10),
+        child: Text('Sexo de ladin com o hygor'),),
+
+        Padding( 
+        padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+        child: Text('sexo com o arthur e bom '),),
+  ])
+
+),
+));
+}
