9087b4223c2b0ec45c28a04eaef4b74f79642dc0
poca pika <126169808+rOBOCOP-GAy@users.noreply.github.com>
2023-03-24 11:52:29 -0300

    Create texto.dart

---
+++ texto.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main(){
+
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+  title: "Primeiro app",
+    home: Row(children: [
+      Text("Texto1"),
+      Text("Texto1"),
+      Text("Texto1"),
+    ],),
+  ));
+
+
+
+}
